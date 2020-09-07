be95e9cc9ff58d0c4c3f8de6de019f04c9484d48
tianse520 <2453974693@qq.com>
2020-09-07 17:13:34 +0800

    添加项目

---
+++ firstgit/src/com/git/project/firstgit.java
@@ -0,0 +1,16 @@
+package com.git.project;
+
+/**
+ * @author tianse
+ */
+public class firstgit {
+    public static void main(String[] args){
+
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
